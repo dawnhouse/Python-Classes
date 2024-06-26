6b710591e2d237254fd8cdbd44f9aaa651058073
dawnhouse <160647857+dawnhouse@users.noreply.github.com>
2024-06-26 23:12:46 +0530

    Add files via upload

---
+++ Variable and DataType/data_types.py
@@ -0,0 +1,68 @@
+""" 
+Variable is the name given to the memory location in a python prpgram.
+Ex: =>
+"""
+a = 10
+b = "Tom"
+c = 12.425
+
+"""Data Types
+
+1 - Integers
+2 - Float
+3 - Booleans
+4 - String
+5 - None
+
+"""
+
+
+"""
+////// RULES FOR DECLARATION OF VARIABLES ///////
+
+
+1- Variables can not be declared with initial as numbers.
+2- Alphabets or underscore can be used as initial characters.
+3- White space is not allowed.
+
+
+
+
+
+
+////////////// OPERATORS IN PYTHON /////////////
+
+1 - Arithmetic Operators => +,-,*,/, %
+
+2 - Assignment Operator => =, += {a = a + 1}, -=(and so on)
+
+3 - Comaprison Operator => ==, <, > != etc.
+
+4 - Logical Operator => and, or, not
+"""
+
+
+"""
+Type()
+
+Input()
+"""
+
+
+a = input("Hello What is your name")
+
+# a = jerry
+
+print(a)
+
+
+
+"""
+1 - Write a python program to add two numbers (with comments)
+2 - Write a python program to find quotient when a number is divide by 2
+3 - Check the type of variable assigned using input() function.
+4 - Use comparison to check wether two variables are equal print the result
+5 - Write a python program to find the average of two numbers entered by the user.
+6 - Write a python program to calcualte the square of a number entered by the user.
+
+"""
