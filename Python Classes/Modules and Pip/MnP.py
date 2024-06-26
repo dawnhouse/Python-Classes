efd91033cee77ca07aebf9c659829070dcf9f133
dawnhouse <160647857+dawnhouse@users.noreply.github.com>
2024-06-26 22:33:16 +0530

    Add files via upload

---
+++ Python Classes/Modules and Pip/MnP.py
@@ -0,0 +1,40 @@
+
+
+# Why Python?
+
+#Featuers of Python
+
+# => Python is a high level language
+# => Free and open source
+# => Portable: Windows, mac, Linux, Ubuntu
+# => Reads like English
+
+
+# Modules => A file that contains a piece of code genrally written by someone else that can be imported and used in our program.
+
+# Types of Modules
+
+# 1 - Pre installed Modules => Built In Modules  (random)
+# 2 - External Modules => These are installed using PIP (tensorflow, pyjokes)
+
+
+
+#PIP => Preferred Installer Program
+
+
+
+# 1 - use pip to install a module and run the code
+
+# REPL = Read Evaluate Print Loop
+
+
+"""
+Single and Multi line Comments
+
+"""
+# 2 - Write any poem in python that is commented out
+# 3 - read about and in the next class demonstrate the use of REPL
+# 4 - Write a python program to print the contents of a directory using the OS module.Search online for the function whis does that.
+
+
+def 
