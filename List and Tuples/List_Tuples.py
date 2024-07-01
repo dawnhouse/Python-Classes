0ef8ff12a6438e0623a854b301d8fec03bfd2d8a
dawnhouse <160647857+dawnhouse@users.noreply.github.com>
2024-07-01 22:19:47 +0530

    Add files via upload

---
+++ List and Tuples/List_Tuples.py
@@ -0,0 +1,28 @@
+# lists = [True, "bear", 45, 45.0, None]
+# #        0       1      2   3     4
+
+# sliced_list = lists[1::3]
+# print(sliced_list)   
+
+# /////////// LIST METHODS ///////////////////
+
+listis= [4,8,2,7,44,5,11,87]
+listis.sort()
+listis.reverse()
+listis.append("This is the last Value")
+listis.insert(6,88)
+listis.pop(0)
+listis.remove("This is the last Value")
+print(listis)
+
+
+
+"""
+1 - Write a program to store seven differnet data types in a list.
+
+2 - Write a program to accept marks of six different students and arrange them (the marks) in a sorted manner.
+start the list with an empty list.
+
+3 - Write a program to sum all the marks that were taken in the 2nd question.
+
+"""
